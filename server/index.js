e23ce7d3d886206d8a6a544650c18bcf59c11d34
Melvrick <melvrickgoh@hotmail.com>
2014-10-10 10:19:48 +0800

    linked in

--- server/index.js
+++ server/index.js
@@ -241,7 +241,7 @@ function _matchAndSearchCompanySingapore(companyName,callback){
 					}else{
 						var searchResults = results1;
 						console.log(searchResults);
-						if (searchResults.companies._total > 0){
+						if (searchResults.companies && searchResults.companies._total > 0){
 							callback(true,linkedIn.extractFirstSearchCompany(searchResults));
 						}else{
 							callback(false,searchResults);

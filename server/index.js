ecd01eb719a4e36648ea219a60bb98adf100d4fb
Melvrick <melvrickgoh@hotmail.com>
2014-10-28 22:35:45 +0800

    hello

--- server/index.js
+++ server/index.js
@@ -23,8 +23,23 @@ var tDAO = new TeamDAO(),
 sDAO = new SupervisorsDAO(),
 iDAO = new IndustriesDAO(),
 wit = new Wit(),
-linkedIn = new LinkedIn(),
-covectric = new Covectric();
+linkedIn = new LinkedIn();
+
+//instantiate the model
+var covectric = require('covectric');
+var model = new covectric.Model();
+
+//populate the vector space and weight tokens based on term frequency
+var id = 1;
+['hello','hi','hi there','hey','hi to you'].forEach(function(t) {
+	model.upsertDocument(id++, t, t);
+});
+model.recomputeVectorBaseTokenWeights();
+
+//search the vector space
+
+var results = model.search("hi bob", 3);
+console.log("Search Results:", results);
 
 /*
 * APP Classes hello

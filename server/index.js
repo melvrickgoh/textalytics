0d07c3a6d693e21b96e7dffd10da6652f88b7f29
Melvrick <melvrickgoh@hotmail.com>
2014-10-28 22:46:07 +0800

    hello

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ var model = new covectric.Model();
 //populate the vector space and weight tokens based on term frequency
 var id = 1;
 ['hello','hi','hi there','hey','hi to you'].forEach(function(t) {
-	model.upsertDocument(id++, t, t);
+    model.upsertDocument(id++, t, t);
 });
 model.recomputeVectorBaseTokenWeights();
 

abddf0a12b29dba08369ed2edc0693d8c43d3470
Melvrick <melvrickgoh@hotmail.com>
2014-10-30 21:49:07 +0800

    hello

--- server/index.js
+++ server/index.js
@@ -222,6 +222,7 @@ main_router.route('/api/recommendation')
 				iDAO.getAllIndustries(function(isSuccess,iResults){
 					var industryDictionary = sortTeamsByIndustries(tResults,sResults,iResults);
 					//console.log(Object.keys(industryDictionary));
+					console.log('----------Industry-------------');
 					console.log(industry);
 					var wantedIndustryGroup;
 					if (industry.indexOf('/')>-1){
